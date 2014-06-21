b32996e3334b2bdec9c7ac273f36b986c39d5eae
Leith Bade <leith@leithalweapon.geek.nz>
2014-06-22 03:18:26 +1000

    Use libuv from git

--- osx/scripts/build_libuv.sh
+++ osx/scripts/build_libuv.sh
@@ -4,18 +4,21 @@ set -o pipefail
 mkdir -p ${PACKAGES}
 cd ${PACKAGES}
 
-if [ ! -f libuv-v${LIBUV_VERSION}.tar.gz ]; then
-    echoerr "downloading libuv: https://github.com/joyent/libuv/archive/v${LIBUV_VERSION}.tar.gz"
-    curl -s -S -f -O -L https://github.com/joyent/libuv/archive/v${LIBUV_VERSION}.tar.gz
-    mv v${LIBUV_VERSION}.tar.gz libuv-v${LIBUV_VERSION}.tar.gz
-else
-    echoerr "using cached node at libuv-v${LIBUV_VERSION}.tar.gz"
-fi
+#if [ ! -f libuv-v${LIBUV_VERSION}.tar.gz ]; then
+#    echoerr "downloading libuv: https://github.com/joyent/libuv/archive/v${LIBUV_VERSION}.tar.gz"
+#    curl -s -S -f -O -L https://github.com/joyent/libuv/archive/v${LIBUV_VERSION}.tar.gz
+#    mv v${LIBUV_VERSION}.tar.gz libuv-v${LIBUV_VERSION}.tar.gz
+#else
+#    echoerr "using cached node at libuv-v${LIBUV_VERSION}.tar.gz"
+#fi
+
+git checkout https://github.com/joyent/libuv.git
 
 echoerr 'building libuv'
-rm -rf libuv-${LIBUV_VERSION}
-tar xf libuv-v${LIBUV_VERSION}.tar.gz
-cd libuv-${LIBUV_VERSION}
+#rm -rf libuv-${LIBUV_VERSION}
+#tar xf libuv-v${LIBUV_VERSION}.tar.gz
+#cd libuv-${LIBUV_VERSION}
+cd libuv
 ./autogen.sh
 ./configure --prefix=${BUILD} --enable-static --disable-shared ${HOST_ARG} \
   --disable-dependency-tracking \
